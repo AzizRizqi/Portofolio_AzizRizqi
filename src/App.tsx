4538cbd8b0d3fa4ce12827e3c526319f4c85cab4
AzizRizqi <azizrizqi2005@gmail.com>
2025-07-14 10:49:23 +0700

    fix link cv

--- src/App.tsx
+++ src/App.tsx
@@ -192,7 +192,7 @@ function App() {
               View My Work
               <ExternalLink className="btn-icon" />
             </a>
-            <a href="/AzizRizqi_Portofolio.github.io/CV_AzizRizqi.pdf" download="CV_AzizRizqi.pdf" className="btn btn-secondary">
+            <a href="/Portofolio_AzizRizqi/CV_AzizRizqi.pdf" download="CV_AzizRizqi.pdf" className="btn btn-secondary">
               Download CV
               <svg className="btn-icon" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m.75 12l3 3m0 0l3-3m-3 3V2.25"></path>
